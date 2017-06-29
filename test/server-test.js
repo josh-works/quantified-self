727b3c7645796a9f60e4cdd59f1c31593728f1b9
Josh Thompson <thompsonjoshd@gmail.com>
2017-06-29 14:22:09 -0600

    first DB test passing

--- test/server-test.js
+++ test/server-test.js
@@ -2,6 +2,11 @@ var assert = require('chai').assert
 var app = require('../server')
 var request = require('request')
 
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('../knexfile')[environment];
+const database = require('knex')(configuration);
+
+
 describe('Server', function(){
   before(function(done){
     this.port = 9876
@@ -25,17 +30,36 @@ describe('Server', function(){
   context('/api/v1', function(){
     describe('GET /foods', function () {
       beforeEach(function(){
-        app.locals.foods = [
-          {id: 1, name: "pizza", calories: 100},
-          {id: 2, name: "banana", calories: 50}
-        ]
+        // do stuff here later
       })
+
+
       it('should return list of foods', function (done) {
         this.request.get('/api/v1/foods', function (error, response) {
           if (error) {done(error)}
           assert.equal(response.statusCode, 200)
           assert.include(response.body, JSON.stringify(app.locals.foods))
           done()
+        })
+      })
+    })
+
+    describe('GET /foods/:id', function () {
+      beforeEach(function(done){
+        database.raw(`INSERT INTO foods (name, calories, created_at) VALUES (?, ?, ?)`,
+          ["bananana", 100, new Date]).then(function () { done() });
+      })
+
+      afterEach(function(done){
+        database.raw(`TRUNCATE foods RESTART IDENTITY`)
+        .then(function () { done() })
+      })
+
+      it('should return 404 if resource is not found', function (done) {
+        this.request.get('/api/v1/foods/10000', function (error, response) {
+          if (error) { done(error) }
+          assert.equal(response.statusCode, 404)
+          done();
         })
       })
     })
